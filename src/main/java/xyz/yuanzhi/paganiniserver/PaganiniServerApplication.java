e394d921063b6bf7f1b00fac6c646de652263436
bezhai <yuanzzhong@outlook.com>
2020-06-15 13:09:56 +0800

    create project

---
+++ src/main/java/xyz/yuanzhi/paganiniserver/PaganiniServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.yuanzhi.paganiniserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaganiniServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaganiniServerApplication.class, args);
+    }
+
+}
