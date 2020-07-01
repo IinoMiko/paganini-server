b36bae238288ca16ff0eb74a9ec740c17ef0a945
bezhai <yuanzzhong@outlook.com>
2020-07-01 10:59:08 +0800

    修复了获取数据乱码问题

--- src/main/java/xyz/yuanzhi/paganiniserver/util/WebMvcConfig.java
+++ src/main/java/xyz/yuanzhi/paganiniserver/util/WebMvcConfig.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
+//@Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("file:F:/Codes/Projects/Java/paganini-server/");
+                .addResourceLocations("file:F:\\Codes\\Projects\\Java\\paganini-server\\");
     }
 }
